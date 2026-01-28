4118b2fdd312b32628ca8bfdef3ace8761b10d77
AnilBojja6990 <anilbojja.dev@gmail.com>
2026-01-28 19:42:59 +0530

    fix

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+import time
 from market_data import get_dummy_candles
 from strategy import calculate_vwap, check_vwap_retest
 from telegram_bot import send_test
 from config import MODE
 
-df = get_dummy_candles()
-df = calculate_vwap(df)
+print("Bot started in", MODE, "mode")
 
-signal = check_vwap_retest(df)
+while True:
+    try:
+        df = get_dummy_candles()
+        df = calculate_vwap(df)
 
-if signal and MODE == "PAPER":
-    send_test()
+        signal = check_vwap_retest(df)
+
+        if signal and MODE == "PAPER":
+            send_test()
+            print("Paper signal sent")
+
+        time.sleep(60)  # check once per minute
+
+    except Exception as e:
+        print("Error:", e)
+        time.sleep(60)
